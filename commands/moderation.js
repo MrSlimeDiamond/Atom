4d8bd93e41d993800fb1ecae64955c29a001171f
MrSlimeDiamond <findlayrichardson3@gmail.com>
2023-04-17 22:16:46 +1200

    feat: /moderation ban

--- commands/moderation.js
+++ commands/moderation.js
@@ -19,6 +19,23 @@ module.exports = {
                         .setRequired(true)
                 )
         )
+        .addSubcommand(subcommand => 
+            subcommand
+            .setName('ban')
+            .setDescription('Ban a member from the server')
+            .addUserOption(option =>
+                option
+                    .setName('user')
+                    .setDescription('User to ban')
+                    .setRequired(true)
+                )
+            .addStringOption(option => 
+                option
+                .setName('reason')
+                .setDescription("The reason for banning the user")
+                .setRequired(false)
+                )
+            )
         .setDefaultMemberPermissions(
             Discord.PermissionFlagsBits.ManageMessages
         ),
@@ -62,6 +79,70 @@ module.exports = {
                 )
 
             await interaction.editReply({ embeds: [embed], ephemeral: true })
+        }
+
+        // /moderation ban
+        if (interaction.options.getSubcommand() == 'ban') {
+            const user = interaction.options.getUser('user')
+            const member = await interaction.guild.members.cache.get(user.id)
+            const reason_ = interaction.options.getString('reason')
+            let reason
+
+            if (!reason_) {
+                reason = 'No reason given'
+            } else {
+                reason = reason_
+            }
+
+            const embed = new Discord.EmbedBuilder()
+            .setColor(0xff0000)
+            .setAuthor({ name: "Banned from " + interaction.guild.name })
+            .setDescription(reason)
+            .setTimestamp()
+
+            const logEmbed = new Discord.EmbedBuilder()
+            .setColor(0x4feb34)
+            .setAuthor({ name: interaction.member.user.tag, iconURL: interaction.member.user.avatarURL() })
+            .setTitle(member.user.tag + " was banned")
+            .setDescription(reason)
+            .setTimestamp()
+
+            interaction.reply({ embeds: [logEmbed] })
+
+            member.createDM()
+            await member.send({ embeds: [embed] })
+            .catch(error => {
+                const embed = new Discord.EmbedBuilder()
+                .setColor(0xff0000)
+                .setAuthor({ name: "Member not accepting DMs" })
+                .setDescription("This member is not accepting DMs")
+
+                interaction.followUp({ embeds: [embed] })
+            })
+            
+            const enabledGuilds = await moduleHandler.getEnabledGuilds('logger')
+            if (!moduleHandler.moduleExists('logger')) {
+                await member.ban({ reason: reason + " banned by " + interaction.member.user.tag})
+                return
+            }
+            if (!enabledGuilds || !enabledGuilds.includes(interaction.guildId)) {
+                await member.ban({ reason: reason + " banned by " + interaction.member.user.tag})
+                return
+            }
+
+            const client = require('../index').client
+
+            const loggerModule = await client.modules.find(mod => mod.name == 'logger')
+            const loggerConfig = require(loggerModule.configLocation)
+
+            const channel = client.channels.cache.get(
+                loggerConfig[interaction.guild.id].logChannel
+            )
+
+            channel.send({ embeds: [logEmbed] })
+
+            // Banning the user should be the last thing we do so that we can DM them first
+            await member.ban({ reason: reason + " banned by " + interaction.member.user.tag})
         }
     },
 }
