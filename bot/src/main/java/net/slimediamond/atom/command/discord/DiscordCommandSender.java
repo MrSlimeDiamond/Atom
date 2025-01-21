d14d72aa114429411e652c971e6b7a3d607f43f2
Findlay Richardson <findlayrichardson3@gmail.com>
2025-01-22 12:17:27 +1300

    Fix IRC help command, adding CommandSender#isAdmin()

--- bot/src/main/java/net/slimediamond/atom/command/discord/DiscordCommandSender.java
+++ bot/src/main/java/net/slimediamond/atom/command/discord/DiscordCommandSender.java
@@ -1,21 +1,33 @@
 package net.slimediamond.atom.command.discord;
 
 import net.dv8tion.jda.api.entities.User;
+import net.slimediamond.atom.Atom;
 import net.slimediamond.atom.command.CommandSender;
+import net.slimediamond.atom.database.Database;
+
+import java.sql.SQLException;
 
 // TODO: Extend JDA command sender
 public class DiscordCommandSender implements CommandSender {
     private String name;
     private User user;
+    private Database database;
 
     public DiscordCommandSender(String name, User user) {
         this.name = name;
         this.user = user;
+
+        this.database = Atom.getServiceManager().getInstance(Database.class);
     }
 
     @Override
     public String getName() {
         return this.name;
+    }
+
+    @Override
+    public boolean isAdmin() throws SQLException {
+        return database.isDiscordAdminByID(this.getId());
     }
 
     public Long getId() {
--- bot/src/main/java/net/slimediamond/atom/irc/commands/HelpCommand.java
+++ bot/src/main/java/net/slimediamond/atom/irc/commands/HelpCommand.java
@@ -2,21 +2,31 @@ package net.slimediamond.atom.irc.commands;
 
 import net.slimediamond.atom.command.CommandContext;
 import net.slimediamond.atom.command.CommandMetadata;
+import net.slimediamond.atom.command.irc.IRCCommand;
 import net.slimediamond.atom.command.irc.IRCCommandContext;
 import net.slimediamond.atom.command.irc.IRCCommandExecutor;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.StringJoiner;
 
 public class HelpCommand implements IRCCommandExecutor {
-    public void execute(IRCCommandContext ctx) {
+    public void execute(IRCCommandContext ctx) throws SQLException {
+        boolean isAdmin = ctx.getSender().isAdmin();
+
         StringJoiner stringJoiner = new StringJoiner(", ");
-        for (CommandMetadata command : ctx.getCommandManager().getCommands()) {
-            // ignoring admin-only commands
-            // TODO: Show admins admin-only commands
-            if(!command.isAdminOnly()) {
-                stringJoiner.add(command.getName());
-            }
-        }
+        ctx.getCommandManager().getCommands().stream()
+                .filter(CommandMetadata::hasIRC)
+                .filter(command -> !command.isAdminOnly() || isAdmin) // Check admin-only commands
+                .filter(command -> {
+                    IRCCommand ircCommand = command.getIRCCommand();
+                    return ircCommand.getWhitelistedChannels().isEmpty() ||
+                            ircCommand.getWhitelistedChannels().contains(ctx.getChannelName());
+                })
+                .map(CommandMetadata::getName) // Get the command name
+                .forEach(stringJoiner::add);
+
+
 
         ctx.reply("My commands are: " + stringJoiner);
     }
