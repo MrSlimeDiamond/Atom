555a09ced60c39a7465d41ea580aa3211f512511
Findlay Richardson <findlayrichardson3@gmail.com>
2025-01-30 09:45:56 +1300

    Moving firstseen/lastseen IRC responses to a shared area

--- bot/src/main/java/net/slimediamond/atom/irc/commands/minecraftonline/MCOLastseen.java
+++ bot/src/main/java/net/slimediamond/atom/irc/commands/minecraftonline/MCOLastseen.java
@@ -1,16 +1,10 @@
 package net.slimediamond.atom.irc.commands.minecraftonline;
 
-import net.slimediamond.atom.command.CommandContext;
 import net.slimediamond.atom.command.irc.IRCCommandContext;
 import net.slimediamond.atom.command.irc.IRCCommandExecutor;
+import net.slimediamond.atom.shared.CommandResponses;
 import net.slimediamond.atom.util.minecraftonline.MCOPlayer;
 import net.slimediamond.atom.util.minecraftonline.exceptions.UnknownPlayerException;
-import org.apache.commons.lang3.time.DurationFormatUtils;
-
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Date;
 
 public class MCOLastseen implements IRCCommandExecutor {
     public void execute(IRCCommandContext ctx) throws Exception {
@@ -20,7 +14,7 @@ public class MCOLastseen implements IRCCommandExecutor {
             MCOPlayer player = new MCOPlayer(username);
 
             player.getLastseen().ifPresentOrElse(lastseen -> {
-                this.sendResponse(player.getName(), lastseen, ctx);
+                CommandResponses.sendLastseenResponse(player.getName(), lastseen, ctx);
             }, () -> {
                 ctx.reply("Could not find that player!");
             });
@@ -29,15 +23,4 @@ public class MCOLastseen implements IRCCommandExecutor {
         }
     }
 
-    private void sendResponse(String username, Date firstseenDate, CommandContext ctx) {
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss, d MMMMM yyyy");
-        String lastseen = formatter.format(firstseenDate);
-
-        Duration firstseenDuration = Duration.between(firstseenDate.toInstant(), Instant.now());
-
-        // TODO: Add years to duration
-        String fromNow = DurationFormatUtils.formatDurationWords(firstseenDuration.toMillis(), true, true) + " ago";
-
-        ctx.reply(username + " last visited Freedonia at " + lastseen + " [" + fromNow + "]");
-    }
 }
