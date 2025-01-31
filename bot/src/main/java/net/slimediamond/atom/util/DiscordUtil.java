2167c78e19f47ce8a37d2032d0569cea1b844da0
Findlay Richardson <findlayrichardson3@gmail.com>
2025-01-31 21:30:27 +1300

    Discord formatting fix

--- bot/src/main/java/net/slimediamond/atom/util/DiscordUtil.java
+++ bot/src/main/java/net/slimediamond/atom/util/DiscordUtil.java
@@ -2,12 +2,6 @@ package net.slimediamond.atom.util;
 
 public class DiscordUtil {
     public static String removeFormatting(String input) {
-        // Remove bold, italic, strikethrough, underlines, and code blocks
-        return input.replaceAll("(?i)\\*\\*([^*]+)\\*\\*", "$1")  // Remove **bold**
-                .replaceAll("(?i)\\*([^*]+)\\*", "$1")       // Remove *italic*
-                .replaceAll("~~([^~]+)~~", "$1")              // Remove ~~strikethrough~~
-                .replaceAll("__([^_]+)__", "$1")              // Remove __underline__
-                .replaceAll("`([^`]+)`", "$1")                // Remove `inline code`
-                .replaceAll("```([^`]+)```", "$1");           // Remove ```code blocks```
+        return input.replaceAll("([*_~`])", "\\\\$1");
     }
 }
