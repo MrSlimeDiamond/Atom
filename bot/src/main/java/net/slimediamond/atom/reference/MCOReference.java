020fd78e259e2ec6848dc6372c48f1ea0e7f897c
Findlay Richardson <findlayrichardson3@gmail.com>
2025-03-17 16:13:20 +1300

    Add FHS to whitelisted MCO Discord guilds

--- bot/src/main/java/net/slimediamond/atom/reference/MCOReference.java
+++ bot/src/main/java/net/slimediamond/atom/reference/MCOReference.java
@@ -4,5 +4,10 @@ public class MCOReference {
     // TODO: Don't hard-code this. Maybe use a database or file
     public static final String[] whitelistedIRC = {"#minecraftonline"};
 
-    public static final Long[] whitelistedDiscord = {696218632618901504L, 288050647998136323L, 972936631558488094L};
+    public static final Long[] whitelistedDiscord = {
+            696218632618901504L, // SlimeDiamond
+            288050647998136323L, // MCO
+            972936631558488094L, // The Forum
+            1045760092718510131L // FHS
+    };
 }
