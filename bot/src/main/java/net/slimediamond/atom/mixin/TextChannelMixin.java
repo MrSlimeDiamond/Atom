136fd89c7c51fa79887db70e59869d6fffb6dd82
Findlay Richardson <findlayrichardson3@gmail.com>
2025-03-31 12:11:23 +1300

    Remove rsync leftovers

--- bot/src/main/java/net/slimediamond/atom/mixin/TextChannelMixin.java
+++
@@ -1,7 +0,0 @@
-package net.slimediamond.atom.mixin;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "type")
-public class TextChannelMixin {
-}
