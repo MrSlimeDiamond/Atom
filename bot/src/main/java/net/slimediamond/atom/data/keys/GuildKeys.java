88317649c91147a862035d67d12ff22df2bc1063
MrSlimeDiamond <findlayrichardson3@gmail.com>
2025-05-09 13:25:50 +1200

    Something I found on my laptop

--- bot/src/main/java/net/slimediamond/atom/data/keys/GuildKeys.java
+++ bot/src/main/java/net/slimediamond/atom/data/keys/GuildKeys.java
@@ -1,7 +1,7 @@
 package net.slimediamond.atom.data.keys;
 
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
+import net.dv8tion.jda.api.entities.emoji.EmojiUnion;
 import net.slimediamond.atom.common.data.ResourceKey;
 import net.slimediamond.data.Key;
 import net.slimediamond.data.value.Value;
@@ -11,6 +11,6 @@ public final class GuildKeys {
     public static final Key<Value<TextChannel>> PINNERINO_CHANNEL = Key.from(ResourceKey.atom("pinnerino_channel"), TextChannel.class);
     public static final Key<Value<TextChannel>> STREAMS_CHANNEL = Key.from(ResourceKey.atom("streams_channel"), TextChannel.class);
     public static final Key<Value<TextChannel>> MEMES_CHANNEL = Key.from(ResourceKey.atom("memes_channel"), TextChannel.class);
-    public static final Key<Value<String>> PINNERINO_EMOJI = Key.from(ResourceKey.atom("pinnerino_emoji"), String.class);
+    public static final Key<Value<EmojiUnion>> PINNERINO_EMOJI = Key.from(ResourceKey.atom("pinnerino_emoji"), EmojiUnion.class);
     public static final Key<Value<Integer>> PINNERINO_THRESHOLD = Key.from(ResourceKey.atom("pinnerino_threshold"), Integer.class);
 }
--- bot/src/main/java/net/slimediamond/atom/discordbot/commands/pinnerino/PinnerinoChannelCommand.java
+++ bot/src/main/java/net/slimediamond/atom/discordbot/commands/pinnerino/PinnerinoChannelCommand.java
@@ -1,10 +1,12 @@
 package net.slimediamond.atom.discordbot.commands.pinnerino;
 
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.slimediamond.atom.command.discord.DiscordCommandContext;
 import net.slimediamond.atom.command.discord.DiscordCommandExecutor;
 import net.slimediamond.atom.common.annotations.GetService;
 import net.slimediamond.atom.data.Database;
+import net.slimediamond.atom.data.keys.GuildKeys;
 import net.slimediamond.atom.util.EmbedUtil;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 public class PinnerinoChannelCommand implements DiscordCommandExecutor {
     @GetService
     private Database database;
+
     @Override
     public void execute(DiscordCommandContext context) throws Exception {
         if (context.getArgs()[0].equals("set")) {
@@ -21,11 +24,14 @@ public class PinnerinoChannelCommand implements DiscordCommandExecutor {
                 context.reply("You need to specify a channel to set");
                 return;
             }
-            GuildChannel channel = channels.get(0);
-            database.setServerPinnerinoChannel(context.getGuild().getIdLong(), channel.getIdLong());
+            if (!(channels.get(0) instanceof TextChannel channel)) {
+                context.replyEmbeds(EmbedUtil.expandedErrorEmbed("Pinnerino channel must be a text channel"));
+                return;
+            }
+            context.getGuild().offer(GuildKeys.PINNERINO_CHANNEL, channel);
             context.replyEmbeds(EmbedUtil.genericSuccessEmbed("Set pinnerino channel"));
         } else if (context.getArgs()[0].equals("unset")) {
-            database.setServerPinnerinoChannel(context.getGuild().getIdLong(), -1L);
+            context.getGuild().offer(GuildKeys.PINNERINO_CHANNEL, null);
             context.replyEmbeds(EmbedUtil.genericSuccessEmbed("Unset pinnerino channel"));
         } else {
             context.replyEmbeds(EmbedUtil.genericIncorrectUsageEmbed("pinnerino channel <set|unset> [<channel>]"));
