6ae70889f4636a8b88e0cf2003cbe9aa127215be
Findlay Richardson <findlayrichardson3@gmail.com>
2025-05-15 22:38:37 +1200

    Add user DAO sql statements, idk if this works

--- bot/src/main/kotlin/net/slimediamond/atom/storage/dao/UserDao.kt
+++ bot/src/main/kotlin/net/slimediamond/atom/storage/dao/UserDao.kt
@@ -4,6 +4,7 @@ import net.slimediamond.atom.Atom
 import net.slimediamond.atom.irc.api.entities.User
 import net.slimediamond.atom.services.PermissionService
 import net.slimediamond.atom.storage.StorageService
+import java.sql.ResultSet
 import java.util.Optional
 
 class UserDao(val id: Int) {
@@ -21,6 +22,42 @@ class UserDao(val id: Int) {
 
     fun hasPermission(permission: String): Boolean {
         return permissionService.hasPermission(this, permission)
+    }
+
+    var discordId: Long?
+        get() {
+            return selectOne("discord_id") { it.getLong("discord_id") }.orElse(null)
+        }
+        set(value) {
+            updateOne("discord_id" to value)
+        }
+
+    var ircNickname: String?
+        get() {
+            return selectOne("irc_nickname") { it.getString("irc_nickname") }.orElse(null)
+        }
+        set(value) {
+            updateOne("irc_nickname" to value)
+        }
+
+    var ircHostname: String?
+        get() {
+            return selectOne("irc_hostname") { it.getString("irc_hostname") }.orElse(null)
+        }
+        set(value) {
+            updateOne("irc_hostname" to value)
+        }
+
+    private fun <T> selectOne(field: String, mapper: (ResultSet) -> T): Optional<T> {
+        return sql.first("SELECT $field FROM users WHERE id = ?", mapper, this.id)
+    }
+
+    private fun <T> updateOne(vararg pairs: Pair<String, T>) {
+        val fields = pairs.joinToString(", ") { "${it.first} = ?" }
+        val values = pairs.map { it.second } + this.id
+        sql.execute("UPDATE users SET $fields WHERE id = ?")
+            .apply { values.forEach { with(it) } }
+            .execute()
     }
 
 }
