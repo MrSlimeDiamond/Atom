b62d805aab805f50e60400ff532a9879c57b53a6
Findlay Richardson <findlayrichardson3@gmail.com>
2025-06-17 13:41:14 +1200

    Javadoc Atom object

--- bot/src/main/kotlin/net/slimediamond/atom/Atom.kt
+++ bot/src/main/kotlin/net/slimediamond/atom/Atom.kt
@@ -5,10 +5,21 @@ import net.slimediamond.atom.configuration.Configuration
 
 object Atom {
 
+    /**
+     * The configuration for the bot
+     */
     @Volatile
     lateinit var configuration: Configuration
+
+    /**
+     * The bot instance
+     */
     @Volatile
     lateinit var bot: Bot
+
+    /**
+     * The [SqlStream] backend powering the [net.slimediamond.atom.storage.StorageService]
+     */
     @Volatile
     lateinit var sql: SqlStream
 
